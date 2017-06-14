febb5b4839423fe9f33d7e145397258e77fe869f
John Zhu <john.zhu@ywystudios.com>
2017-06-14 08:11:49 -0700

    add save

--- src/main/com/ywy/editor/TableServices.java
+++ src/main/com/ywy/editor/TableServices.java
@@ -2,7 +2,6 @@ package com.ywy.editor;
 
 import org.glassfish.grizzly.http.server.Request;
 import org.glassfish.grizzly.http.server.Session;
-import org.json.HTTP;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -11,6 +10,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -129,27 +129,50 @@ public class TableServices {
     @Produces(MediaType.APPLICATION_JSON)
     public Response saveTableData(@Context Session session, @PathParam("name") String tableName, String objstr ) {
         if (connString != null) {
+            ArrayList<String> dataFields = new ArrayList<String>();
+            ArrayList<String> autoFields = new ArrayList<String>();
+
             try(Connection conn = DriverManager.getConnection(connString)) {
-
+                Set<String> pFields = getPrimaryKeyColumnsForTable(conn, tableName);
                 Statement s = conn.createStatement();
-                try (ResultSet rs = s.executeQuery("SELECT * FROM " + tableName)) {
-                    JSONObject retobj = new JSONObject();
-                    retobj.put("table", tableName);
-                    JSONArray json = new JSONArray();
-
-                    while (rs.next()) {
-                        JSONObject obj = new JSONObject();
-                        for(int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
-                            obj.put(rs.getMetaData().getColumnName(i), rs.getObject(i));
+                try (ResultSet rs = s.executeQuery("SELECT * FROM " + tableName + " where  1=0")) {
+                    ResultSetMetaData metaData = rs.getMetaData();
+                    for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                        String colName = metaData.getCatalogName(i);
+                        if (metaData.isAutoIncrement(i)) {
+                            autoFields.add(colName);
+                        } else if (!pFields.contains(colName)) {
+                            dataFields.add(colName);
+                        }
+                    }
+                }
+                JSONObject obj = new JSONObject(objstr);
+                JSONArray data = obj.getJSONArray("data");
+                if (data != null) {
+                    for(int i = 0 ; i < data.length(); i++) {
+                        JSONObject rec = data.getJSONObject(i);
+                        boolean insert = true;
+                        for (String a : autoFields) {
+                            if (rec.get(a) != null && rec.getInt(a) != 0) {
+                                //update statement
+                                insert = false;
+                                break;
+                            }
 
                         }
-                        json.put(obj);
+                        if (insert) {
+                            String sql = "INSERT INTO " + tableName + "(" + String.join("," , dataFields) + ")";
+                        }
+                        else {
 
+                        }
+                        conn.commit();
                     }
-                    retobj.put("data", json);
-                    return Response.ok(retobj.toString()).build();
+
                 }
-            } catch (SQLException e) {
+                PreparedStatement st = conn.prepareStatement(sql);
+            }
+            catch (SQLException e) {
                 e.printStackTrace();
             }
         }
